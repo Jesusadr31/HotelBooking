52e3423efd2cdf7a3c6fd481ccdc1d9915e4678b
Jesus <jesus.angulo@correo.unimet.edu.ve>
2024-03-13 20:08:30 -0400

    Primer commit

---
+++ src/hotelbookingproyect/HotelBookingProyect.java
@@ -0,0 +1,17 @@
+
+package hotelbookingproyect;
+
+/**
+ *
+ * @author Jesús
+ */
+public class HotelBookingProyect {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
