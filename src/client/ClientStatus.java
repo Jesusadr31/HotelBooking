6a8d795803ee1bcc39b20928af38e9ede9039f8b
Jesus <jesus.angulo@correo.unimet.edu.ve>
2024-03-15 22:05:31 -0400

    Agregando clases List, NodeList, Global, ClientHistory, ClientStatus

--- src/client/ClientStatus.java
+++ src/client/ClientStatus.java
@@ -3,8 +3,100 @@ package client;
 
 /**
  *
- * @author chris
+ * @author jesus
  */
-public class ClientStatus {
+public class ClientStatus{
+    
+    private String roomnum;
+    private String name;
+    private String lastname;
+    private String mail;
+    private String gender;
+    private String phoneNumber;    
+    private String arriveDate;
+   
+    public ClientStatus(){
+    
+    }
+    
+    public ClientStatus(String roomnum,String name,String lastname,String mail,String gender,String phoneNumber,String arriveDate){
+        this.roomnum = roomnum;
+        this.name = name;
+        this.lastname = lastname;
+        this.mail = mail;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.arriveDate = arriveDate;
+    }
+    
+    public String showInfo() {
+        String result = "";
+        result += "Numero Habitacion :" + roomnum + "\n";
+        result += "Nombre y apellido: " + name + " " + lastname + "\n";
+        result += "Email: " + mail + "\n";
+        result += "Género: " + gender + "\n";
+        result += "Telefono" + phoneNumber + "\n";
+        result += "Llegada: " + arriveDate + "\n";
+        return result;
+    }
+
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getArriveDate() {
+        return arriveDate;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setArriveDate(String arriveDate) {
+        this.arriveDate = arriveDate;
+    }
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getRoomnum() {
+        return roomnum;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setRoomnum(String roomnum) {
+        this.roomnum = roomnum;
+    }
+    
     
 }
--- src/list/NodeList.java
+++ src/list/NodeList.java
@@ -1,13 +1,79 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package list;
+
+import client.ClientHistory;
+import client.ClientStatus;
+import client.Rooms;
 
 /**
  *
- * @author chris
+ * @author Jesus
  */
 public class NodeList {
+    private ClientHistory clienth;
+    private Rooms rooms;
+    private ClientStatus clients;
+    private int roomNum;
+    private NodeList next;
+    
+    public NodeList(ClientHistory clienth){
+        this.clienth = clienth;
+        this.next = null;
+    }
+   
+    
+    public NodeList(Rooms rooms){
+        this.rooms = rooms;
+        this.next = null;
+    }
+    
+    public NodeList(ClientStatus clients){
+        this.clients = clients;
+        this.next = null;
+    }
+    
+    public NodeList(int roomNum){
+        this.roomNum = roomNum;
+        this.next = null;
+    }
+
+    public int getRoomNum() {
+        return roomNum;
+    }
+
+    public void setRoomNum(int roomNum) {
+        this.roomNum = roomNum;
+    }
+    
+    
+    public ClientHistory getClienth() {
+        return clienth;
+    }
+
+    public NodeList getNext() {
+        return next;
+    }
+
+    public void setClienth(ClientHistory clienth) {
+        this.clienth = clienth;
+    }
+
+    public void setNext(NodeList next) {
+        this.next = next;
+    }
+
+
+    public Rooms getRooms() {
+        return rooms;
+    }
+
+
+    public void setRooms(Rooms rooms) {
+        this.rooms = rooms;
+    }
+
+    public ClientStatus getClients() {
+        return clients;
+    }
     
 }
